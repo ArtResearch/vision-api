30e791973f5e69525fcd93874d6e1af1a48e233e
mafragias <mafragias@hotmail.com>
2021-01-27 11:36:15 +0200

    updates 27-01-2021

--- ingore/clearLocalIndexes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo
-for ID in {1..130}
-do
-   curl -X DELETE localhost:4212/index/images/$ID
-done
--- ingore/deleteIndexesPastec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo
-for ID in {1000000..1002000}
-do
-   curl -X DELETE http://vision.artresearch.net:4212/index/images/$ID
-done
