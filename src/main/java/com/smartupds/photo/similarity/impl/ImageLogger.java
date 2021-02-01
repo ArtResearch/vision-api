7b871d8410f8e8cd29b28bcba6a40752edf3d433
mafragias <mafragias@hotmail.com>
2021-02-01 18:10:01 +0200

    run both methods with the same query

--- src/main/java/com/smartupds/photo/similarity/impl/ImageLogger.java
+++ src/main/java/com/smartupds/photo/similarity/impl/ImageLogger.java
@@ -7,6 +7,7 @@ package com.smartupds.photo.similarity.impl;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.smartupds.photo.similarity.common.Resources;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -40,10 +41,12 @@ public class ImageLogger {
         try {
             Gson GSON = new GsonBuilder().setPrettyPrinting().create();
             JSONObject errorsJsonObject = new JSONObject();
+            Logger.getLogger(ImageLogger.class.getName()).log(Level.INFO, "Creating Log file for {0}", Resources.SIMILARITY_METHOD);
             OutputStreamWriter writer =new OutputStreamWriter(new FileOutputStream(this.log), "UTF-8");
             errorsJsonObject.put("errors", parseJSON());
             writer.append(GSON.toJson(errorsJsonObject));
             writer.close();
+            Logger.getLogger(ImageLogger.class.getName()).log(Level.INFO, "Log file located at : {0}", this.log);
         } catch (FileNotFoundException | UnsupportedEncodingException ex) {
             Logger.getLogger(ImageLogger.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -55,7 +58,7 @@ public class ImageLogger {
     private JSONArray parseJSON(){
         JSONArray errorJsonObject = new JSONArray();
         try {
-            // IMAGE_DOWNLOADER_HTTP_ERROR
+            // IMAGE_DOWNLOADER_HTTP_ERROR || IMAGE_NOT_DECODED
             JSONParser parserJson = new JSONParser();
             Object obj = parserJson.parse(new InputStreamReader(new FileInputStream(json), "UTF8"));
             JSONObject jsonObject = (JSONObject) obj;
@@ -63,8 +66,17 @@ public class ImageLogger {
             Iterator<JSONObject> iterator = imageList.iterator();
             while(iterator.hasNext()){
                 JSONObject result = iterator.next();
-                if (result.get("image_id")==null || ((JSONObject)result.get("search_results")).get("type").equals("IMAGE_DOWNLOADER_HTTP_ERROR")){
-                    errorJsonObject.add(result);
+                if (Resources.SIMILARITY_METHOD.equals(Resources.PASTEC_METHOD)){
+                    if (result.get("image_id")==null || ((JSONObject)result.get("search_results")).get("type").equals("IMAGE_DOWNLOADER_HTTP_ERROR") 
+                            || ((JSONObject)result.get("search_results")).get("type").equals("IMAGE_NOT_DECODED")){
+                        errorJsonObject.add(result);
+                    }
+                } else if (Resources.SIMILARITY_METHOD.equals(Resources.MATCH_METHOD)){
+                    if ( !result.isEmpty() && ( result.get("image_id")==null 
+                            || !((JSONObject)((JSONArray)result.get("search_results")).get(0)).get("status").equals("ok")
+                            || !((JSONArray)((JSONObject)((JSONArray)result.get("search_results")).get(0)).get("error")).isEmpty() )){
+                        errorJsonObject.add(result);
+                    }
                 }
             }
         } catch (FileNotFoundException | UnsupportedEncodingException ex) {
