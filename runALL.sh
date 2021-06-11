dc51a88937278ea571583f716eeb91f25c36d953
Lukas Klic <lklic@itatti.harvard.edu>
2021-06-11 14:30:37 +0000

    updated

---
+++ runALL.sh
@@ -0,0 +1,16 @@
+#/!/bin/bash
+
+
+./run_Pastec.sh configZeriCheat.conf
+
+
+./run_Pastec.sh configItattiCheat.conf
+
+
+./run_Pastec.sh configHertzianaCheat.conf
+
+
+./run_Pastec.sh configKHICheat.conf
+
+
+./run_Pastec.sh configMarburgCheat.conf
