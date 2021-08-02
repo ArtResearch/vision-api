3d79040aee9fd922728feafbaef6a44e71966b47
Lukas Klic <lklic@itatti.harvard.edu>
2021-08-02 13:07:27 +0000

    configs

--- runALL.sh
+++ runALL.sh
@@ -1,16 +1,27 @@
 #/!/bin/bash
 
+echo "Run Zeri"
 
 ./run_Pastec.sh configZeriCheat.conf
 
+echo "Run ITatti"
 
 ./run_Pastec.sh configItattiCheat.conf
 
+echo "Run Hertziana"
 
 ./run_Pastec.sh configHertzianaCheat.conf
 
+echo "Run KHI"
 
 ./run_Pastec.sh configKHICheat.conf
 
+echo "Run Marburg"
 
 ./run_Pastec.sh configMarburgCheat.conf
+
+echo "Run Frick"
+
+./run_Pastec.sh configFrickCheat.conf
+
+echo "END"
